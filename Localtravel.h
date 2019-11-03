dfdeaa0605e26a5099ae624a9688f3de6f7ed223
Rishit <51650467+rishittripathi-therocking@users.noreply.github.com>
2019-11-03 17:53:47 +0530

    Add files via upload

---
+++ Localtravel.h
@@ -0,0 +1,98 @@
+extern int Local_travel(){
+system("cls");
+  int local,price;
+  printf("______________________________________________________________________________________\n");
+  printf("______________________________________________________________________________________\n");
+  printf("||                                                                                  ||\n");
+  printf("||                                                                                  ||\n");
+  printf("||                                                                                  ||\n");
+  printf("||                              Enter Your choice:-                                 ||\n");
+  printf("||                                1:- Home station                                  ||\n");
+  printf("||                                2:- DESTINATION                                   ||\n");
+  printf("||                                                                                  ||\n");
+  printf("||                                                                                  ||\n");
+  printf("______________________________________________________________________________________\n");
+  printf("______________________________________________________________________________________\n");
+  printf("PLease Enter Your LOcal Travel\n");
+  int home;
+    scanf("%d",&local);
+    switch(local){
+      case 1:
+  while (1){system("cls");
+  printf("______________________________________________________________________________________\n");
+  printf("______________________________________________________________________________________\n");
+  printf("||                                                                                  ||\n");
+  printf("||                                                                                  ||\n");
+  printf("||                                                                                  ||\n");
+  printf("||                              Enter Your choice:-                                 ||\n");
+  printf("||                          1:- To and From RLy station(Rs 150 for cab)             ||\n");
+  printf("||                          2:- To and From AIRPORT (Rs 250 for cab)                ||\n");
+  printf("||                          3:- Exit                                                ||\n");
+  printf("||                                                                                  ||\n");
+  printf("______________________________________________________________________________________\n");
+  printf("______________________________________________________________________________________\n");
+  printf("Enetr your choice ");
+  scanf("%d",&home);
+  switch(home){
+    case 1:price+=150;break;
+    case 2:price+=250;break;
+  }
+  if(home==3){
+    break;
+  }
+  }
+  break;
+    case 2:
+  while(1){system("cls");
+            printf("______________________________________________________________________________________\n");
+  printf("______________________________________________________________________________________\n");
+  printf("||                                                                                  ||\n");
+  printf("||                                                                                  ||\n");
+  printf("||                                                                                  ||\n");
+  printf("||                              Enter Your choice:-                                 ||\n");
+  printf("||                          1:- To and From RLy station(Rs 250 for cab)             ||\n");
+  printf("||                          2:- To and From AIRPORT (Rs 350 for cab)                ||\n");
+  printf("||                          3:- SITESEING (Rs 550 for cab)                          ||\n");
+  printf("||                          4:- EXIT                                                ||\n");
+  printf("||                                                                                  ||\n");
+  printf("______________________________________________________________________________________\n");
+  printf("______________________________________________________________________________________\n");
+  printf("Enetr your choice ");
+
+        scanf("%d",&home);
+  switch(home){
+    case 1:price+=250;break;
+    case 2:price+=350;break;
+    case 3:price+=550;break;
+  }
+  if(home==4){
+    break;
+  }
+  }
+  break;
+    }
+
+  system("cls");
+printf("\n");
+printf("________________________________________________________________________________________\n");
+  printf("________________________________________________________________________________________\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                         WELCOME TO ARMIS TOURS AND TRAVELS                         ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                Enter your choices:                                 ||\n");
+  printf("||                                 1:-  ENTER YOUR DATA                               ||\n");
+  printf("||                                 2:-  TICKETING                                     ||\n");
+  printf("||                                 3:-  STAY IN HOTEL/RESORT                          ||\n");
+  printf("||                                 4:-  LOCAL TRAVEL                                  ||\n");
+  printf("||                                 5:-  EXTRA ACTIVITIES                              ||\n");
+  printf("||                                 6:-  EXIT                                          ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("________________________________________________________________________________________\n");
+  printf("________________________________________________________________________________________\n");
+  return price;
+}
