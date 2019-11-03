516b95f0fe04475ebb04e2d0344cd002cc67b4d0
Rishit <Rishittripathi13@gmail.com>
2019-11-03 16:39:26 +0530

    Main File

---
+++ main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "data.h"
+#include "ticketing.h"
+//#include "Localtravel.c"
+//#include "StayInhotel.c"
+//#include "Extraactivities.c"
+//#include "data.txt"
+int main()
+{
+  printf("________________________________________________________________________________________\n");
+  printf("________________________________________________________________________________________\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                         WELCOME TO ARMIS TOURS AND TRAVELS                         ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                Enter your choices:                                 ||\n");
+  printf("||                                 1:-  ENTER YOUR DATA                               ||\n");
+  printf("||                                 2:-  TICKETING                                     ||\n");
+  printf("||                                 3:-  STAY IN HOTEL/RESORT                          ||\n");
+  printf("||                                 4:-  LOCAL TRAVEL                                  ||\n");
+  printf("||                                 5:-  EXTRA ACTIVITIES                              ||\n");
+  printf("||                                 6:-  EXIT                                          ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("||                                                                                    ||\n");
+  printf("________________________________________________________________________________________\n");
+  printf("________________________________________________________________________________________\n");
+  int choice,price;
+while(1){
+        scanf("%d",&choice);
+  switch(choice){
+     case 1: data();break;
+     case 2: price+=ticketing();break;
+  }
+  if(choice==6){
+    break;
+  }
+}
+printf("YOUR TOTAL TRAVEL COST IS=%d\n\n\n\n\n\n",price);
+}
